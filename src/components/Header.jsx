7837fc1808fe6021639bf0514eaba69c5678d14c
Kailash Singh <singhkailash.sk9@gmail.com>
2024-01-21 20:19:55 +0530

    Create Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,15 @@ function Header(){
             <h1>
                 Blog Website
             </h1>
+            <nav>
+                <ul className="navbar">
+                    <input type="text" name="search" id="searchbar" placeholder="Type to Search..." />
+                    <li>Home</li>
+                    <li>Explore</li>
+                    <li>Profile</li>
+                    <li>Settings</li>
+                </ul>
+            </nav>
         </header>
     </div>
 }
